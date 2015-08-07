e8db64d9e200128411199259f2080937dff0eba3
Fatih Metin <fth.mtn@gmail.com>
2015-08-08 01:03:39 +0300

    object hw

---
+++ Object.java
@@ -0,0 +1,8 @@
+
+public class Object {
+	public static void main(String[] args) {
+	
+		System.out.println(java.lang.Object.class.getSuperclass());
+		System.out.println("Null döner çünkü üst sınıf yok.");
+	}
+}
